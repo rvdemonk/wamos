65d1482f5d1aa7e5bfb6b793df95ad3808e31c96
rvdemonk <rvdemonk@gmail.com>
2022-11-06 16:37:12 +1000

    test setup bugs

--- test/WamosV1.t.sol
+++ test/WamosV1.t.sol
@@ -13,22 +13,23 @@ contract WamosV1Test is Test {
         0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
     uint256 MINT_PRICE = 0.01 ether;
 
-    VRFCoordinatorV2Interface vrfCoordinator;
+    VRFCoordinatorV2Mock vrfCoordinator;
     WamosV1 wamos;
-    uint96 subscriptionId;
+    uint64 subscriptionId;
 
     function setUp() public {
         // set up coordinator mock
         vrfCoordinator = new VRFCoordinatorV2Mock(BASE_FEE, GAS_PRICE_LINK);
         subscriptionId = vrfCoordinator.createSubscription();
-        vrfCoordinator.fundSubscription(subscriptionId);
         // deploy WamosV1
-        wamos = WamosV1(
+        wamos = new WamosV1(
             address(vrfCoordinator),
             VRF_MOCK_KEYHASH,
             subscriptionId,
             MINT_PRICE
         );
+        // fund subscription
+        vrfCoordinator.fundSubscription(subscriptionId, SUB_FUNDING);
     }
 
     function testIncrement() public {}
