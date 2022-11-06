be3b2ec64481449d0bc42e135afab7f45bd2aba4
rvdemonk <rvdemonk@gmail.com>
2022-11-06 14:38:36 +1000

    meta functions and trait generation scaffold

--- src/WamosV1.sol
+++ src/WamosV1.sol
@@ -10,38 +10,40 @@ import "chainlink-v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "./WamosRandomnessV0.sol";
 
 struct Ability {
-    uint8 Type;
-    uint8 Attack;
-    uint8 Defence;
-    uint8 MagicAttack;
-    uint8 MagicDefence;
-    uint8 Speed;
-    uint8 Accuracy;
-    uint8 PP;
-    uint8 Cooldown;
+    uint256 Type;
+    uint256 Attack;
+    uint256 Defence;
+    uint256 MagicAttack;
+    uint256 MagicDefence;
+    uint256 Speed;
+    uint256 Accuracy;
+    uint256 PP;
+    uint256 Cooldown;
 }
 
-struct WamoData {
-    uint256 id;
-    uint8 Health;
-    uint8 Attack;
-    uint8 Defence;
-    uint8 MagicAttack;
-    uint8 MagicDefence;
-    uint8 Stamina;
-    uint8 Mana;
-    uint8 Luck;
+struct WamoTraits {
+    uint256 Health;
+    uint256 Attack;
+    uint256 Defence;
+    uint256 MagicAttack;
+    uint256 MagicDefence;
+    uint256 Stamina;
+    uint256 Mana;
+    uint256 Luck;
 }
 
 struct SpawnRequest {
     bool exists;
-    bool fulfilled;
+    bool randomnessFulfilled;
+    bool completed;
     uint256 randomWord;
     address sender;
     uint256 tokenId;
 }
 
-error WamoSpawnRequestNotFound(uint256 requestId);
+error SpawnRequestNotFound(uint256 requestId);
+error SpawnRequestNotFulfilled(uint256 requestId);
+error WamoAlreadySpawned(uint256 tokenId);
 
 contract WamosV0 is ERC721, VRFConsumerBaseV2 {
     //// META CONSTANTS
@@ -50,6 +52,7 @@ contract WamosV0 is ERC721, VRFConsumerBaseV2 {
 
     address public contractOwner;
     uint256 public tokenCount;
+    uint256 public mintPrice;
 
     // VRF COORDINATOR
     VRFCoordinatorV2Interface vrfCoordinator;
@@ -63,22 +66,31 @@ contract WamosV0 is ERC721, VRFConsumerBaseV2 {
 
     // WAMO SPAWN REQUEST STORAGE
     mapping(uint256 => SpawnRequest) spawnRequests;
+    mapping(uint256 => uint256) tokenIdToRandomnWord;
+    mapping(uint256 => uint256) tokenIdToSpawnRequest;
     uint256[] requestIds;
     uint256 lastRequestId;
 
     // WAMO DATA
-    mapping(uint256 => WamoData[]) attributes;
-    mapping(uint256 => Ability[]) abilities;
+    mapping(uint256 => WamoTraits) wamoIdToTraits;
+    mapping(uint256 => Ability[]) wamoIdToAbilities;
+
+    // EVENTS
+    event SpawnRequested(uint256 requestId, uint256 indexed tokenId, address sender);
+    event SpawnCompleted(uint256 requestId, uint256 indexed tokenId, address sender);
+    event RandomnessFulfilled(uint256 requestId, uint256 tokenId);
 
     constructor(
         address _vrfCoordinatorAddr,
         bytes32 _vrfKeyHash,
-        uint64 _vrfSubscriptionId
+        uint64 _vrfSubscriptionId,
+        uint256 _mintPrice
     ) ERC721(NAME, SYMBOL) VRFConsumerBaseV2(_vrfCoordinatorAddr) {
         contractOwner = msg.sender;
         vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinatorAddr);
         vrfKeyHash = _vrfKeyHash;
         vrfSubscriptionId = _vrfSubscriptionId;
+        mintPrice = _mintPrice;
 
         vrfNumWords = 1;
         vrfCallbackGasLimit = 100000;
@@ -91,43 +103,78 @@ contract WamosV0 is ERC721, VRFConsumerBaseV2 {
     }
 
     /**
-     * @dev First stage of mint
-        Wamo request is made, VRF request sent, request stored, event emitted
+     * @notice Stage 1 of wamo mint
+     * Wamo request is made, VRF request sent, request stored, event emitted
+     * @custom:decision return request id or token id? does this matter with two-way mapping?
      */
-    function requestWamoSpawn() public payable returns (uint256 requestId) {
-        uint256 tokenId = tokenCount;
+    function requestSpawnWamo() public payable returns (uint256 tokenId) {
+        require(msg.value >= mintPrice, "Insufficient payment.");
+        // assign token id for new wamo
+        tokenId = tokenCount;
         tokenCount++;
         // request randomness (from the gods)
-        requestId = vrfCoordinator.requestRandomWords(
+        uint256 requestId = vrfCoordinator.requestRandomWords(
             vrfKeyHash,
             vrfSubscriptionId,
             vrfRequestConfirmations,
             vrfCallbackGasLimit,
             vrfNumWords
         );
-        // store request
+        // store request, including token id of requested wamo
         spawnRequests[requestId] = SpawnRequest({
             exists: true,
-            fulfilled: false,
+            randomnessFulfilled: false,
+            completed: false,
             randomWord: 0,
             sender: msg.sender,
             tokenId: tokenId
         });
+        tokenIdToSpawnRequest[tokenId] = requestId;
         requestIds.push(requestId);
         lastRequestId = requestId;
-        // TODO emit event
+        // TODO 
+        emit SpawnRequested(requestId, tokenId, msg.sender);
+        return tokenId;
     }
 
-    // TODO
-    function claimRequestedWamo() public payable returns (uint256 tokenId) {
-        // generate wamo traits
+    /**
+     * @notice Stage 2 of wamo mint
+     * @param tokenId the id of the token requested for which to generate traits and mint to request sender
+     */
+    function completeSpawnWamo(uint256 tokenId) public payable {
+        require(tokenId > tokenCount, "This token id has not been minted yet!");
+        uint256 requestId = tokenIdToSpawnRequest[tokenId]; 
+        // check request has not already been fulfilled
+        if (spawnRequests[requestId].completed) {
+            revert WamoAlreadySpawned(tokenId);
+        }
+        // check vrf request fulfilled
+        if (!spawnRequests[requestId].randomnessFulfilled) {
+            revert SpawnRequestNotFulfilled(requestId);
+        }
+        uint256 randomWord = spawnRequests[requestId].randomWord;
+        wamoIdToTraits[tokenId] = generateWamoTraits(randomWord);
+        address owner = spawnRequests[requestId].sender;
+        _safeMint(owner, tokenId);
+    }
+
+    /**
+     * @dev public visibility for testing and experimenting
+     */
+    function generateWamoTraits(uint256 randomWord) public pure returns (WamoTraits memory traits) {
+        traits.Health = (randomWord % 100) + 1;
+        return traits;
+    }
+
+    function setMintPrice(uint256 _mintPrice) public onlyOwner {
+        mintPrice = _mintPrice;
     }
 
     function withdrawFunds() public payable onlyOwner {
         payable(contractOwner).transfer(address(this).balance);
     } 
 
-    function tokenURI(uint256 id) public view override returns (string memory) {
+    function tokenURI(uint256 id) public pure override returns (string memory) {
         return Strings.toString(id);
     }
 
@@ -137,10 +184,11 @@ contract WamosV0 is ERC721, VRFConsumerBaseV2 {
     ) internal override {
         // check request exists
         if (!spawnRequests[_requestId].exists) {
-            revert WamoSpawnRequestNotFound(_requestId);
+            revert SpawnRequestNotFound(_requestId);
         }
-        spawnRequests[_requestId].fulfilled = true;
+        spawnRequests[_requestId].randomnessFulfilled = true;
         spawnRequests[_requestId].randomWord = _randomWords[0];
-        // TODO emit event
+        uint256 tokenId = spawnRequests[_requestId].tokenId;
+        emit RandomnessFulfilled(_requestId, tokenId);
     }
 }
