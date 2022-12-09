a10d2bf1bf9cc49a8ec179c6237a09e4769726ab
rvdemonk <rvdemonk@gmail.com>
2022-12-09 13:09:54 +1000

    tidy

--- models/breeding_sim.py
+++ models/breeding_sim.py
@@ -22,13 +22,36 @@ MU = 50
 SIGMA = 16
 N = 1000
 
+class WamoSimulator:
 
-def create_gen(mu, sigma, n):
-    """
-    A generation of wam0s with normally distributed traits
-    """
-    population = [np.random.normal(mu, sigma, len(TRAITS)) for wamo in range(n)]
-    return population
+    def __init__(self, mu, sigma, n):
+        self.mu = mu
+        self.sigma = sigma
+        self.n = n
+        self.population = 
+
+    def display(w, population):
+        print("\n -- displaying traits of Wam0 #", w)
+        for i,trait in enumerate(TRAITS):
+            print(f"{population[w][i]} | {trait}")
+            
+
+    def breed(w1, w2, population):
+        new_traits = []
+        for i,trait in enumerate(TRAITS):
+            mu = (population[w1][i] + population[w2][i]) / 2
+            sigma = abs(population[w1][i] - population[w2][i]) / 2
+            score = np.random.normal(mu, sigma, 1)
+            new_traits.append(score)
+        return new_traits
+
+
+    def create_gen(mu, sigma, n):
+        """
+        A generation of wam0s with normally distributed traits
+        """
+        population = [np.random.normal(mu, sigma, len(TRAITS)) for wamo in range(n)]
+        return population
 
 
 def main():
