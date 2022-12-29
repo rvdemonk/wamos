94a9e885ef3dc6ce0ba0e949d769a198ae05aa95
rvdemonk <rvdemonk@gmail.com>
2022-12-29 12:34:40 +1000

    started current_world task

---
+++ tasks/current_world.js
@@ -0,0 +1,4 @@
+task("current_world", "Logs information about the current wamos contract system")
+  .setAction(async (taskArgs, hre) => {
+    const helpers = require('../scripts/v2/helpers');
+  })
