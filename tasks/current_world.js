67291fe70cdacb0c7b5edd745b63bb594e6b99d6
rvdemonk <rvdemonk@gmail.com>
2022-12-29 14:10:22 +1000

    current_world logging

--- tasks/current_world.js
+++ tasks/current_world.js
@@ -3,5 +3,9 @@ task("current_world", "Logs information about the current wamos contract system"
     const helpers = require('../scripts/v2/helpers');
     const [ wamos, arena ] = await helpers.getContracts();
 
+
+    console.log(`\n--- Active Wamos World ----`)
     console.log('WamosV2:', wamos.address.substring(0,6));
+    console.log('ArenaV2:', arena.address.substring(0,6))
+    console.log('\n');
   })
