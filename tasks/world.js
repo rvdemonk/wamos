c530f580c804f328320504cb321aac1ac3f2537f
rvdemonk <rvdemonk@gmail.com>
2022-12-30 09:39:26 +1000

    wamo count added to world task

--- tasks/world.js
+++ tasks/world.js
@@ -2,7 +2,8 @@ task("world", "Logs information about the current wamos contract system")
   .setAction(async (taskArgs, hre) => {
     const helpers = require('../scripts/v2/helpers');
     const [ wamos, arena ] = await helpers.getContracts();
-
+    const wamoCount = (await wamos.nextTokenId()) - 1;
+    
 
     console.log(`\n--- Active Wamos World ----`)
     console.log('WamosV2:', wamos.address.substring(0,6));
