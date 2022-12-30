ee37a076adf0fc0a59b6a274bc34a788f62f910e
rvdemonk <rvdemonk@gmail.com>
2022-12-30 11:24:20 +1000

    fixed function name bug in world task

--- tasks/world.js
+++ tasks/world.js
@@ -2,10 +2,11 @@ task("world", "Logs information about the current wamos contract system")
   .setAction(async (taskArgs, hre) => {
     const helpers = require('../scripts/v2/helpers');
     const [ wamos, arena ] = await helpers.getContracts();
-    const wamoCount = (await wamos.nextTokenId()) - 1;
-
+    const wamoCount = (await wamos.nextWamoId()) - 1;
+    // get timestamps
     console.log(`\n--- Active Wamos World ----`)
     console.log('WamosV2:', wamos.address.substring(0,6));
     console.log('ArenaV2:', arena.address.substring(0,6))
+    console.log('wamo population:', wamoCount);
     // console.log('\n');
   })
