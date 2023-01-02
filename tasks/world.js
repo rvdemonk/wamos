8c0b89056f3f734bc5abec1cfed1f3e126e3826d
rvdemonk <rvdemonk@gmail.com>
2023-01-02 19:59:05 +1000

    adding owners to world task

--- tasks/world.js
+++ tasks/world.js
@@ -3,13 +3,16 @@ task("world", "Logs information about the current wamos contract system")
     const helpers = require('../scripts/helpers');
     const wamos = await helpers.getWamos();
     const arena = await helpers.getArena();
+    const wamosOwner = await wamos.contractOwner();
+    const arenaOwner = await arena.deployer();
     const wamoCount = (await wamos.nextWamoId()) - 1;
     const wamosTime = await wamos.timestamp();
     const arenaTime = await arena.timestamp();
     // get timestamps
     console.log(`\n--- Active Wamos World ----`)
-    console.log('WamosV2:', wamos.address.substring(0,6));
-    console.log('ArenaV2:', arena.address.substring(0,6))
+    console.log('WamosV2:', wamos.address.substring(0,10));
+    console.log('ArenaV2:', arena.address.substring(010));
+    console.log(`Owner of contracts: [${wamosOwner.substring(0,6)}, ${arenaOwner.substring(0,6)}]`);
     console.log('wamo population:', wamoCount);
     console.log(`wamos deployed @ ${new Date(wamosTime*1000)}`);
     console.log(`arena deployed @ ${new Date(arenaTime*1000)}\n`);
