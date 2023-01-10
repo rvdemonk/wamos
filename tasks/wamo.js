7a1726fcf699146f6acb6850f5d6d7b9382333a3
rvdemonk <rvdemonk@gmail.com>
2023-01-10 15:35:59 +1000

    replaced reference to depracated helpers in mint task

--- tasks/wamo.js
+++ tasks/wamo.js
@@ -1,15 +1,18 @@
 task("wamo", "Displays the data and traits of the wamo with the given id")
   .addParam("id", "The Wamo's ID number")
   .setAction(async (taskArgs, hre) => {
+    const utils = require("../scripts/utils/");
+
     const id = taskArgs.id;
     console.log(`Wamo ID: ${taskArgs.id}`);
-    
-    const helpers = require('../scripts/helpers');
-    const wamos = await helpers.getWamos();
+
+    const wamos = await utils.getters.getWamos();
     const tokenCount = (await wamos.nextWamoId()) - 1;
     if (id > tokenCount) {
-        throw new Error(`Wamo #${id} has not been minted yet! Token count is ${tokenCount}`)
+      throw new Error(
+        `Wamo #${id} has not been minted yet! Token count is ${tokenCount}`
+      );
     }
     const traits = await wamos.getTraits(id);
     helpers.displayWamoTraits(id, traits);
-})
+  });
